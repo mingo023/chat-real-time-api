4fa8fafc794beeaa534dcaf92fd97b323138d97d
Minh Ngo <nvminh023@gmail.com>
2019-01-12 18:15:50 +0700

    message features update

--- validation/index.js
+++ validation/index.js
@@ -1,2 +1,3 @@
 exports.user = require('./user');
 exports.group = require('./group');
+exports.message = require('./message');
---
+++ validation/message.js
@@ -0,0 +1,29 @@
+import Joi from 'joi';
+
+module.exports.create = {
+  body: {
+    messeages: Joi.string().min(1).max(255),
+    group: Joi.string().regex(/^[0-9a-fA-F]{24}$/, 'Id must to be the ObjectId')
+  }
+};
+
+module.exports.get = {
+  params: {
+    id: Joi.string().regex(/^[0-9a-fA-F]{24}$/, 'Id must to be the ObjectId').required()
+  }
+}
+
+module.exports.update = {
+  params: {
+    id: Joi.string().regex(/^[0-9a-fA-F]{24}$/, 'Id must to be the ObjectId')
+  },
+  body: {
+    messeages: Joi.string().min(1).max(255),
+  }
+}
+
+module.exports.delete = {
+  params: {
+    id: Joi.string().regex(/^[0-9a-fA-F]{24}$/, 'Id must to be the ObjectId')
+  }
+}
